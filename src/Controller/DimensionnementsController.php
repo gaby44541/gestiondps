20c43495d83d92aa206bb8e8aba8742885244d95
Gabriel Boursier <gabriel.boursier@loire-atlantique.protection-civile.org>
2019-09-19 19:45:49 +0200

    Traduction de certains messages anglais vers français

--- src/Controller/DimensionnementsController.php
+++ src/Controller/DimensionnementsController.php
@@ -13,7 +13,7 @@ use Cake\Utility\Hash;
  */
 class DimensionnementsController extends AppController
 {
-	var $navigation = [ 
+	var $navigation = [
 		[ 'label' => 'New dimensionnement', 'config' => [ 'controller' => 'Dimensionnements', 'action' => 'add' ]],
 		[ 'label' => 'List dimensionnement', 'config' => [ 'controller' => 'Dimensionnements', 'action' => 'index' ]],
 		[ 'label' => 'List Demandes', 'config' => ['controller' => 'Demandes', 'action' => 'index' ]],
@@ -21,7 +21,7 @@ class DimensionnementsController extends AppController
 		[ 'label' => 'List Dispositifs', 'config' => ['controller' => 'Dispositifs', 'action' => 'index' ]],
 		[ 'label' => 'Add Dispositifs', 'config' => ['controller' => 'Dispositifs', 'action' => 'add' ]],
     ];
-	
+
     /**
      * Index method
      *
@@ -33,7 +33,7 @@ class DimensionnementsController extends AppController
             'contain' => ['Demandes']
         ];
         $dimensionnements = $this->paginate($this->Dimensionnements);
-		
+
 		$navigation = $this->navigation;
 
         $this->set(compact('dimensionnements','navigation'));
@@ -44,46 +44,46 @@ class DimensionnementsController extends AppController
      *
      * @return \Cake\Http\Response|void
      */
-    public function wizard() 
+    public function wizard()
     {
-		$demande_id = (int) $this->Wizard->getDatas('Demandes.id'); 
+		$demande_id = (int) $this->Wizard->getDatas('Demandes.id');
 
 		$dimensionnements = $this->Dimensionnements->find('all')
 			->contain(['Demandes','Dispositifs'])
 			->where(['demande_id'=>$demande_id])
 			->orderAsc('horaires_debut')
 			->orderAsc('horaires_fin');
-		
+
 		$dimensionnements_ids = Hash::extract($dimensionnements->toArray(),'{n}.id');
-		
-		$this->Wizard->injectDatas('Dimensionnements.id',$dimensionnements_ids); 
-		
+
+		$this->Wizard->injectDatas('Dimensionnements.id',$dimensionnements_ids);
+
 		$demande = $this->Dimensionnements->Demandes->get($demande_id);
-		
+
 		$navigation = $this->navigation;
 
         $this->set(compact('dimensionnements','navigation','demande','demande_id'));
     }
 
 	public function ajax($function = false){
-		
+
 		$this->autoRender = false;
-		
+
 	    // Force le controller à rendre une réponse JSON.
         $this->RequestHandler->renderAs($this, 'json');
-		
+
         // Définit le type de réponse de la requete AJAX
         $this->response->type('application/json');
 
         // Chargement du layout AJAX
         $this->viewBuilder()->layout('ajax');
-		
+
 		// Chargement des données
 		$json_data[] = ['id'=>2500,'title'=>'test','start'=>'2018-07-16 12:30:00','end'=>'2018-07-18 05:45:00','url'=>'http://localhost/crud/antennes/view/1'];
 		$json_data[] = ['id'=>2501,'title'=>'test','start'=>'2018-07-18 05:45:00','end'=>'2018-07-27'];//,'rendering'=>'background'
 
 		$response = $this->response->withType('json')->withStringBody(json_encode($json_data));
-		
+
 		// Retour des données encodées en JSON
 		return $response;
 	}
@@ -114,19 +114,19 @@ class DimensionnementsController extends AppController
     {
 
         $dimensionnement = $this->Dimensionnements->newEntity();
-		
+
         if ($this->request->is('post')) {
-            
+
 			$dimensionnement = $this->Dimensionnements->patchEntity($dimensionnement, $this->request->getData());
-			
+
             if ($this->Dimensionnements->save($dimensionnement)) {
-                $this->Flash->success(__('The dimensionnement has been saved.'));
+                $this->Flash->success(__('Le dimensionnement a bien été sauvegardé.'));
 
                 return $this->redirect(['action' => 'index']);
             }
-			
+
             $this->Flash->error(__('The dimensionnement could not be saved. Please, try again.'));
-        
+
 		}
 
 		if(!empty($demande_id)){
@@ -134,10 +134,10 @@ class DimensionnementsController extends AppController
 		} else {
 			$demandes = $this->Dimensionnements->Demandes->listing();
 		}
-		
+
 		$navigation = $this->navigation;
 		$environnements = $dimensionnement->get('environnements');
-		
+
         $this->set(compact('dimensionnement','demandes','navigation','demande_id','environnements'));
     }
 
@@ -156,13 +156,13 @@ class DimensionnementsController extends AppController
         if ($this->request->is(['patch', 'post', 'put'])) {
             $dimensionnement = $this->Dimensionnements->patchEntity($dimensionnement, $this->request->getData());
             if ($this->Dimensionnements->save($dimensionnement)) {
-                $this->Flash->success(__('The dimensionnement has been saved.'));
+                $this->Flash->success(__('Le dimensionnement a bien été sauvegardé.'));
 
                 return $this->redirect(['action' => 'index']);
             }
             $this->Flash->error(__('The dimensionnement could not be saved. Please, try again.'));
         }
-		
+
         //$demandes = $this->Dimensionnements->Demandes->find('list', ['limit' => 200]);
 		$demandes = $this->Dimensionnements->Demandes->alone($dimensionnement->demande_id);
 
@@ -170,10 +170,10 @@ class DimensionnementsController extends AppController
 		$dimensionnement->secours_presents = explode(',',$dimensionnement->secours_presents);
 		$dimensionnement->documents_officiels = explode(',',$dimensionnement->documents_officiels);
 		$dimensionnement->acces = explode(',',$dimensionnement->acces);
-		
+
 		$navigation = $this->navigation;
 		$environnements = $dimensionnement->get('environnements');
-		
+
         $this->set(compact('dimensionnement', 'demandes','navigation','environnements'));
     }
 
@@ -205,7 +205,7 @@ class DimensionnementsController extends AppController
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
     public function duplicate($id = null)
-    {	
+    {
 		$this->autoRender = false;
         //$this->request->allowMethod(['post', 'delete']);
 
@@ -217,7 +217,7 @@ class DimensionnementsController extends AppController
 
        return $this->redirect(['action' => 'index']);
     }
-	
+
 
     /**
      * Delete method
@@ -227,47 +227,47 @@ class DimensionnementsController extends AppController
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
     public function scinder($id = null)
-    {	
+    {
 		$this->autoRender = false;
         //$this->request->allowMethod(['post', 'delete']);
 
         $dimensionnement = $this->Dimensionnements->get($id, [
             'contain' => ['Demandes', 'Dispositifs']
         ]);
-		
+
 		$old = $dimensionnement;
-		
+
 		$demande_id = (int) $dimensionnement->get('demande_id');
-		
+
 		if( $demande_id ){
-			
+
 			$data = $this->Dimensionnements->Demandes->duplicate($demande_id);
-			
+
 			if( $data ){
-				
+
 				$demande_id = $data->get('id');
-				
+
 				if( $demande_id ){
 
 					$dimensionnement->set('demande_id',$demande_id);
-					
+
 					$save = $this->Dimensionnements->patchEntity($old,$dimensionnement->toArray());
-					
+
 					if($this->Dimensionnements->save($save)) {
-						
-						$this->Flash->success(__('The dimensionnement has been saved.'));
+
+						$this->Flash->success(__('Le dimensionnement a bien été sauvegardé.'));
 						return $this->redirect(['controller'=>'demandes','action' => 'view',$demande_id]);
 					}
 				}
-		
+
 			}
-			
+
 			$this->Flash->error(__('Echec de duplication de la demande attachée à cette déclaration.'));
-			
+
 		}
-		
+
 		$this->Flash->error(__('Impossible de scinder cette déclaration.'));
-		
+
 		return $this->redirect(['controller'=>'demandes','action' => 'index']);
 
     }
